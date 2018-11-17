f037b70ef25be76ab56979cf3e8df888255e022a
Art Beckman <aabeckman@yahoo.com>
2018-11-17 11:18:29 -0800

    git rm --cached config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
-    'facebook': {
-            clientId: '303567110203165',
-            clientSecret: 'e07cdb9dcdfbd9c4d3275637cdee9d6a'
-    }
-}
